a98ecc9a0f619818ee8db378beb535d45ae66e49
unknown <viki8880@gmail.com>
2023-12-17 16:34:02 +0530

    added the prject and typewrite

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -124,4 +124,47 @@ $(document).ready(function(){
         });
 
 });	
+
+
+/*----------typewrite ------------------- */
+
+const typedText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["Web Developer", "1+ Year of Work Experience", "Learner..."];
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+const erase = () => {
+  if (charIndex > 0) {
+    cursor.classList.remove('blink');
+    typedText.textContent = textArray[textArrayIndex].slice(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 80);
+  } else {
+    cursor.classList.add('blink');
+    textArrayIndex++;
+    if (textArrayIndex > textArray.length - 1) {
+      textArrayIndex = 0;
+    }
+    setTimeout(type, 1000);
+  }
+}
+
+const type = () => {
+  if (charIndex <= textArray[textArrayIndex].length - 1) {
+    cursor.classList.remove('blink');
+    typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 120);
+  } else {
+    cursor.classList.add('blink');
+    setTimeout(erase, 1000);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+})
 	
